6c9ece707fe1da6f700c9b9a8bbefeb96d018e6b
EmmaJennings <ejennings13@my.bcit.ca>
2026-01-26 18:37:00 -0800

    Changed CSS

--- src/app/page.js
+++ src/app/page.js
@@ -18,36 +18,49 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles.intro}>
-          <a href="/about" className={styles.link}>About</a>
-          <a href="/projects" className={styles.link}>Projects</a>
-          <h1 style={{ 
-            color: '#5C82B9', 
-            fontWeight: 700, 
-            fontSize: '3rem', 
-            margin: '0.8rem' }}>Hi there, I'm Emma!
+        <div className={styles.header}>
+            <img
+              src="/emma_logo.png"
+              alt="Emma Jennings Logo"
+              className={styles.logo}
+              height="80px"
+            />
+          <h1 className={styles.title}
+            style={{ 
+              color: '#5C82B9', 
+              fontWeight: 700, 
+              fontSize: '3rem', 
+              margin: '0.8rem' 
+            }}>
+              Hi there, I'm Emma!
           </h1>
+        </div>
+          <div className={styles.nav}>
+            <a href="/projects" className={styles.link}>My Projects</a>
+            <a href="/about" className={styles.link}>Beep Me</a>
+          </div>
+        <div className={styles.intro}>
           <img
             src="/emma1.jpeg"
             alt="Photo of Emma"
             height="300px"
             className={fadeInFirst ? styles["fade-in"] : ""}
-            style={{ transform: 'rotate(-10deg)' }}
+            style={{ transform: 'rotate(-10deg)', 
+              position: 'relative', 
+              left: '45rem',
+              }}
           />
           <img
             src="/kenai.JPG"
             alt="Photo of my dog Kenai"
             height="300px"
             className={fadeInSecond ? styles["fade-in"] : styles["hidden-until-fade"]}
-            style={{ transform: 'rotate(10deg)' }}
+            style={{ transform: 'rotate(10deg)',
+              position: 'relative',
+              left: '25rem',
+              top: '-10rem',
+            }}
           />
-        </div>
-        <div className={styles.description}>
-          <h2 style={{ 
-            fontWeight: 700, 
-            fontSize: '1.4rem', 
-            margin: '0.8rem' }}>Welcome to my Portfolio :)
-          </h2>
         </div>
       </main>
 
