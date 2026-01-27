a9a39cd2065b688c04accbac31351ae0ad7e2c02
Emma <ejennings13@my.bcit.ca>
2026-01-26 17:41:41 -0800

    Delete .history directory

--- .history/src/app/about/page_20260112154817.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: '#fff', color: '#000' }}>
-      <h1 style={{ fontWeight: 700, fontSize: '2rem' }}>About</h1>
-      <p style={{ marginTop: 16, fontSize: '1.1rem' }}>This is a placeholder About page for Emma Jennings' portfolio wireframe.</p>
-    </main>
-  );
-}
