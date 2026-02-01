60e829b9471b59ececf49ce303f0b3d861c33799
EmmaJennings <ejennings13@my.bcit.ca>
2026-01-31 16:53:20 -0800

    Adding description to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -10,10 +10,19 @@ export default function Home() {
     return (
         <div className={styles.page}>
         <NavBar></NavBar>
-
         <main className={styles.main}>
-            <div >
-                
+            <div className={styles.description}>
+              <h2 className={styles.h2}>Am I coming off as immature to you?</h2>
+              <br></br>
+              <p>
+                I love the study of art and design, I find it fascinating how 
+                humans interact with art, not only in our day to day lives,
+                but how we make it a part of ourselves.
+                <br></br>
+                <br></br>
+                I'm a Designer and Web Developer based in Vancouver, BC.
+              
+              </p>
             </div>
         </main>
 
