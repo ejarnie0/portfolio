be23ee305b3505840031f82fcd1c7d813d06a640
EmmaJennings <ejennings13@my.bcit.ca>
2026-02-02 15:00:05 -0800

    Removed line i forgot to from navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -35,9 +35,6 @@ export default function NavBar() {
     // Change this default title to whatever you want on the home page (/)
     const pageTitle = activeItem?.title ?? "Hi there, I'm Emma!";
 
-    <div className={styles.logoWrap}>
-  <img src="/emma_logo.png" className={styles.logo} alt="Emma Jennings Logo" />
-</div>
     return (
         <header className={styles.header}>
         <div className={styles.brand}>
